9c3b5f9aeaab98d75f9701891154f45b0f19d532
RdWeirdo981 <75655022+RdWeirdo981@users.noreply.github.com>
2021-06-30 10:05:39 +0800

    Delete 2021S1 FIT5225 Cloud Computing and Security/A2/html/html directory

--- 2021S1 FIT5225 Cloud Computing and Security/A2/html/html/js/config.js
+++
@@ -1,8 +0,0 @@
-window._config = {
-    cognito: {
-        userPoolId: 'us-east-1_r0kCM2sZK', // e.g. us-east-2_uXboG5pAb
-        region: 'us-east-1', // e.g. us-east-2
-		clientId: 'n1k3c90olirfe9isfng6cmrkj' //is this used anywhere?
-    },
-};
-
